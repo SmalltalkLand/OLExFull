7312cc58ef2d6591875ab17e36039a35231bb318
Graham <gkgoat6700@gmail.com>
2020-01-22 19:46:33 -0500

    more stuff

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@
 import {chromeSwitch} from '../../OLEx/dist/index.js'
 //@ts-ignore
 import theContent from 'extract-loader!./content.ts'
+//@ts-ignore
+import OLOSClass from '../../olos/dist/index.js'
